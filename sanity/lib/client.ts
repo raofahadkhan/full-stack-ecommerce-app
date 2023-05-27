c6d0ad43fe13f84d109d96727e8f14a4aac0bb9a
RAO FAHAD <raofahad046@gmail.com>
2023-05-27 14:48:44 +0500

    added environment variable and created sanity client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,11 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+// import { apiVersion, dataset, projectId, useCdn } from "../env";
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+	token: process.env.NEXT_SANITY_API_TOKEN,
+	apiVersion: "2023-05-26",
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	projectId: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	useCdn: true,
+});
