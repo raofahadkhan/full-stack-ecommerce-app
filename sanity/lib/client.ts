3c76fe0eb0b44d8300d85d87ec56d21605530942
RAO FAHAD <raofahad046@gmail.com>
2023-05-27 23:36:00 +0500

    removed wrong project id issue

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
 	token: process.env.NEXT_SANITY_API_TOKEN,
 	apiVersion: "2023-05-26",
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-	projectId: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	useCdn: true,
 });
