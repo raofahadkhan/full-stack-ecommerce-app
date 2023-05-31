f5e593ad4501af9d9948daadbfb3b4843527f047
RAO FAHAD <raofahad046@gmail.com>
2023-06-01 00:49:20 +0500

    created a basic product card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { urlForImage } from "../../sanity/lib/image";
 
 const getProductData = async () => {
 	const res = await client.fetch(`*[_type=="product"]{
+		name,
 		price,
 		_id,
 		title,
@@ -18,6 +19,7 @@ const getProductData = async () => {
 
 interface IProduct {
 	title: string;
+	name: string;
 	_id: string;
 	description: string;
 	price: number;
@@ -30,15 +32,20 @@ interface IProduct {
 export default async function Home() {
 	const data: IProduct[] = await getProductData();
 	return (
-		<div>
+		<div className="grid grid-cols-[repeat(3,auto)] justify-center gap-x-10">
 			{data.map((item: IProduct) => (
 				<div>
 					<Image
 						src={urlForImage(item.image[0]).url()}
 						alt="product-card"
+						className="max-h-[250px] object-cover object-top"
 						width={300}
 						height={300}
 					/>
+					<h2>{item.name}</h2>
+					<h3>${item.price}</h3>
+					<button className="border py-2 px-6 rounded bg-blue-600 text-white
+					">Add to Cart</button>
 				</div>
 			))}
 		</div>
