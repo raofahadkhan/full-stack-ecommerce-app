ca00e54981aa2ba299d14216ae8798572df5da54
RAO FAHAD <raofahad046@gmail.com>
2023-06-01 01:41:53 +0500

    drizzle orm setup and created the cartTable schema

---
+++ src/lib/drizzle.ts
@@ -0,0 +1,12 @@
+import { pgTable, varchar, serial, integer } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+
+const cartTable = pgTable("cart", {
+	id: serial("id").primaryKey(),
+	user_id: varchar("user_id", { length: 255 }).notNull(),
+	product_id: varchar("product_id", { length: 255 }).notNull(),
+	quantity: integer("quantity").notNull(),
+});
+
+export const db = drizzle(sql)
