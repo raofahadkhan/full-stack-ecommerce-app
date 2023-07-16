1c22b2ed0f3333ba7bac3d7c921ae52aaee04f3f
RAO FAHAD <raofahad046@gmail.com>
2023-07-16 13:40:02 +0500

    updated client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-	token: process.env.NEXT_SANITY_API_TOKEN,
+	// token: process.env.NEXT_SANITY_API_TOKEN,
 	apiVersion: "2023-05-26",
 	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
